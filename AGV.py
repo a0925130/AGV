2ae47c6b42bac3cc19b3941a8572348f62dff9b1
Cat <a0925130@gmail.com>
2021-08-06 15:43:57 +0800

    新增 Laser 和 map

--- AGV.py
+++ AGV.py
@@ -5,123 +5,14 @@ import time
 import pybullet_data
 import math
 import matplotlib.pyplot as plt
-
-physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-p.setGravity(0, 0, -9.81)
-# planeId = p.loadURDF("ur_mir_data/plane.urdf")
-planeId = p.loadURDF("plane.urdf")
-roomStartPos = [0, 0, 1]
-obstacleSratPos_1 = [3.5, 2.5, 1]
-obstacleSratPos_2 = [-3, 1, 1]
-obstacleSratPos_3 = [3, -4, 1]
-obstacleSratPos_4 = [6, 0, 1]
-obstacleSratPos_5 = [-3, -4, 1]
-obstacleSratPos_6 = [-5, -2, 1]
-
-room = p.loadURDF("ur_mir_data/room.urdf", globalScaling=5, basePosition=roomStartPos)
-obstacle_1 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=obstacleSratPos_1)
-obstacle_2 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=obstacleSratPos_2)
-obstacle_3 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=obstacleSratPos_3)
-obstacle_4 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=obstacleSratPos_4)
-obstacle_5 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=obstacleSratPos_5)
-obstacle_6 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=obstacleSratPos_6)
-robot = p.loadURDF("ur_mir_data/mir_ur.urdf")
-
 point_x = []
 point_y = []
-numRays = 32
+numRays = 16
 rayLen = 10
 rayColor = [0, 0, 1]
 
-numJoints = p.getNumJoints(robot)
-for joint in range(numJoints):
-    print(p.getJointInfo(robot, joint))
-    p.setJointMotorControl(robot, joint, p.POSITION_CONTROL, 0, 100)
 
-
-def Camera(robot_id: int, robot_link: int, width: int = 224, height: int = 224):
-    # basePos, baseOrientation = p.getBasePositionAndOrientation(robot_id)
-    basePos = p.getLinkState(robot_id, robot_link)[0]
-    baseOrientation = p.getLinkState(robot_id, robot_link)[1]
-    matrix = p.getMatrixFromQuaternion(baseOrientation)
-    tx_vec = np.array([matrix[0], matrix[3], matrix[6]])
-    tz_vec = np.array([matrix[2], matrix[5], matrix[8]])
-
-    basePos = np.array(basePos)
-    targetPos = basePos + 1 * tx_vec
-
-    viewMatrix = p.computeViewMatrix(
-        cameraEyePosition=basePos,
-        cameraTargetPosition=targetPos,
-        cameraUpVector=tz_vec,
-    )
-    projectionMatrix = p.computeProjectionMatrixFOV(
-        fov=50.0,
-        aspect=1.0,
-        nearVal=0.01,
-        farVal=20,
-    )
-
-    # width, height, rgbImg, depthImg, segImg =
-    p.getCameraImage(
-        width=width, height=height,
-        viewMatrix=viewMatrix,
-        projectionMatrix=projectionMatrix,
-    )
-    # return width, height, rgbImg, depthImg, segImg
-
-
-def Laser(robot_id):
-    results = []
-    rayFrom_front = []
-    rayFrom_back = []
-    rayTo_front = []
-    rayTo_back = []
-    for i in range(numRays):
-        Camera(robot, 28)
-        basePos_front = p.getLinkState(robot_id, 3)[0]
-        ray_front_x = basePos_front[0] + rayLen * math.sin(2. * math.pi * float(i) / numRays)
-        ray_front_y = basePos_front[1] + rayLen * math.cos(2. * math.pi * float(i) / numRays)
-        ray_front_z = basePos_front[2]
-        rayFrom_front.append(basePos_front)
-        rayTo_front.append([ray_front_x, ray_front_y, ray_front_z])
-        p.addUserDebugLine(rayFrom_front[i], rayTo_front[i], rayColor)
-        results.append(p.rayTest(rayFrom_front[i], rayTo_front[i]))
-
-        basePos_back = p.getLinkState(robot_id, 4)[0]
-        ray_back_x = basePos_back[0] + rayLen * math.sin(2. * math.pi * float(i) / numRays)
-        ray_back_y = basePos_back[1] + rayLen * math.cos(2. * math.pi * float(i) / numRays)
-        ray_back_z = basePos_back[2]
-        rayFrom_back.append(basePos_back)
-        rayTo_back.append([ray_back_x, ray_back_y, ray_back_z])
-        p.addUserDebugLine(rayFrom_back[i], rayTo_back[i], rayColor)
-        results.append(p.rayTest(rayFrom_back[i], rayTo_back[i]))
-
-        for j in range(len(results)):
-            if basePos_front[0] > basePos_back[0]:
-                if basePos_front[1] > basePos_back[1]:
-                    if not ((basePos_front[0] > results[j][0][3][0]) & (basePos_back[0] < results[j][0][3][0])) & ((basePos_front[1] > results[j][0][3][1]) & (basePos_back[1] < results[j][0][3][1])):
-                        point_x.append(results[j][0][3][0])
-                        point_y.append(results[j][0][3][1])
-                else:
-                    if not ((basePos_front[0] > results[j][0][3][0]) & (basePos_back[0] < results[j][0][3][0])) & ((basePos_front[1] < results[j][0][3][1]) & (basePos_back[1] > results[j][0][3][1])):
-                        point_x.append(results[j][0][3][0])
-                        point_y.append(results[j][0][3][1])
-            else:
-                if basePos_front[1] > basePos_back[1]:
-                    if not ((basePos_front[0] < results[j][0][3][0]) & (basePos_back[0] > results[j][0][3][0])) & ((basePos_front[1] > results[j][0][3][1]) & (basePos_back[1] < results[j][0][3][1])):
-                        point_x.append(results[j][0][3][0])
-                        point_y.append(results[j][0][3][1])
-                else:
-                    if not ((basePos_front[0] < results[j][0][3][0]) & (basePos_back[0] > results[j][0][3][0])) & ((basePos_front[1] < results[j][0][3][1]) & (basePos_back[1] > results[j][0][3][1])):
-                        point_x.append(results[j][0][3][0])
-                        point_y.append(results[j][0][3][1])
-    p.removeAllUserDebugItems()
-
-
-def map(x, y):
+def mapping(x, y):
     plt.scatter(x, y, color='blue', marker='o')
     plt.ion()
     plt.show()
@@ -129,22 +20,130 @@ def map(x, y):
     plt.clf()
 
 
-def start():
-    Thread(target=map(point_x, point_y))
-    Laser(robot)
-    start()
+class mir_with_ur:
+    def __init__(self):
+        physicsClient = p.connect(p.GUI)
+        p.configureDebugVisualizer(p.COV_ENABLE_GUI, 1)
+        p.setAdditionalSearchPath(pybullet_data.getDataPath())
+        p.setGravity(0, 0, -9.81)
+        self.obstacleSratPos_1 = [3.5, 2.5, 1]
+        self.roomStartPos = [0, 0, 1]
+        self.planeId = p.loadURDF("plane.urdf")
+        self.obstacleSratPos_2 = [-3, 1, 1]
+        self.obstacleSratPos_3 = [3, -4, 1]
+        self.obstacleSratPos_4 = [6, 0, 1]
+        self.obstacleSratPos_5 = [-3, -4, 1]
+        self.obstacleSratPos_6 = [-5, -2, 1]
+        self.room = p.loadURDF("ur_mir_data/room.urdf", globalScaling=5, basePosition=self.roomStartPos)
+        self.obstacle_1 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=self.obstacleSratPos_1)
+        self.obstacle_2 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=self.obstacleSratPos_2)
+        self.obstacle_3 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=self.obstacleSratPos_3)
+        self.obstacle_4 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=self.obstacleSratPos_4)
+        self.obstacle_5 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=self.obstacleSratPos_5)
+        self.obstacle_6 = p.loadURDF("ur_mir_data/obstacle.urdf", basePosition=self.obstacleSratPos_6)
+        self.robot = p.loadURDF("ur_mir_data/mir_ur.urdf")
+        p.setRealTimeSimulation(1)
+        numJoints = p.getNumJoints(self.robot)
+        for joint in range(numJoints):
+            print(p.getJointInfo(self.robot, joint))
+            p.setJointMotorControl(self.robot, joint, p.POSITION_CONTROL, 0, 100)
+        # p.setJointMotorControl2(self.robot, 8, p.VELOCITY_CONTROL, targetVelocity=-15)
+        # p.setJointMotorControl2(self.robot, 7, p.VELOCITY_CONTROL, targetVelocity=-15)
+
+    def Camera(self):
+            basePos = p.getLinkState(self, 28)[0]
+            baseOrientation = p.getLinkState(self, 28)[1]
+            matrix = p.getMatrixFromQuaternion(baseOrientation)
+            tx_vec = np.array([matrix[0], matrix[3], matrix[6]])
+            tz_vec = np.array([matrix[2], matrix[5], matrix[8]])
+
+            basePos = np.array(basePos)
+            targetPos = basePos + 1 * tx_vec
+
+            viewMatrix = p.computeViewMatrix(
+                cameraEyePosition=basePos,
+                cameraTargetPosition=targetPos,
+                cameraUpVector=tz_vec,
+            )
+            projectionMatrix = p.computeProjectionMatrixFOV(
+                fov=50.0,
+                aspect=1.0,
+                nearVal=0.01,
+                farVal=20,
+            )
+
+            p.getCameraImage(
+                width=214, height=214,
+                viewMatrix=viewMatrix,
+                renderer=p.ER_BULLET_HARDWARE_OPENGL,
+                projectionMatrix=projectionMatrix,
+            )
+
+    def Laser(self):
+        rayFrom_front = []
+        rayFrom_back = []
+        rayTo_front = []
+        rayTo_back = []
+        for i in range(numRays):
+            results = []
+            basePos_front = p.getLinkState(self, 3)[0]
+            ray_front_x = basePos_front[0] + rayLen * math.sin(2. * math.pi * float(i) / numRays)
+            ray_front_y = basePos_front[1] + rayLen * math.cos(2. * math.pi * float(i) / numRays)
+            ray_front_z = basePos_front[2]
+            rayFrom_front.append(basePos_front)
+            rayTo_front.append([ray_front_x, ray_front_y, ray_front_z])
+            p.addUserDebugLine(rayFrom_front[i], rayTo_front[i], rayColor)
+            results.append(p.rayTest(rayFrom_front[i], rayTo_front[i]))
+
+            basePos_back = p.getLinkState(self, 4)[0]
+            ray_back_x = basePos_back[0] + rayLen * math.sin(2. * math.pi * float(i) / numRays)
+            ray_back_y = basePos_back[1] + rayLen * math.cos(2. * math.pi * float(i) / numRays)
+            ray_back_z = basePos_back[2]
+            rayFrom_back.append(basePos_back)
+            rayTo_back.append([ray_back_x, ray_back_y, ray_back_z])
+            p.addUserDebugLine(rayFrom_back[i], rayTo_back[i], rayColor)
+            results.append(p.rayTest(rayFrom_back[i], rayTo_back[i]))
+
+            for j in range(len(results)):
+                if basePos_front[0] > basePos_back[0]:
+                    if basePos_front[1] > basePos_back[1]:
+                        if not ((basePos_front[0] > results[j][0][3][0]) & (basePos_back[0] < results[j][0][3][0])) & (
+                                (basePos_front[1] > results[j][0][3][1]) & (basePos_back[1] < results[j][0][3][1])):
+                            point_x.append(results[j][0][3][0])
+                            point_y.append(results[j][0][3][1])
+                    else:
+                        if not ((basePos_front[0] > results[j][0][3][0]) & (basePos_back[0] < results[j][0][3][0])) & (
+                                (basePos_front[1] < results[j][0][3][1]) & (basePos_back[1] > results[j][0][3][1])):
+                            point_x.append(results[j][0][3][0])
+                            point_y.append(results[j][0][3][1])
+                else:
+                    if basePos_front[1] > basePos_back[1]:
+                        if not ((basePos_front[0] < results[j][0][3][0]) & (basePos_back[0] > results[j][0][3][0])) & (
+                                (basePos_front[1] > results[j][0][3][1]) & (basePos_back[1] < results[j][0][3][1])):
+                            point_x.append(results[j][0][3][0])
+                            point_y.append(results[j][0][3][1])
+                    else:
+                        if not ((basePos_front[0] < results[j][0][3][0]) & (basePos_back[0] > results[j][0][3][0])) & (
+                                (basePos_front[1] < results[j][0][3][1]) & (basePos_back[1] > results[j][0][3][1])):
+                            point_x.append(results[j][0][3][0])
+                            point_y.append(results[j][0][3][1])
+        p.removeAllUserDebugItems()
+
+    def run(self):
+
+        try:
+            while True:
+                do_laser = Thread(target=mir_with_ur.Laser, args=(self.robot,))
+                do_camera = Thread(target=mir_with_ur.Camera, args=(self.robot,))
+                do_laser.start()
+                do_camera.start()
+                mapping(point_x, point_y)
+                do_laser.join()
+                do_camera.join()
+        finally:
+            p.disconnect()
 
 
-# p.setJointMotorControl2(boxId, 21, p.POSITION_CONTROL, targetVelocity=-30)
-# p.setJointMotorControl2(boxId, 22, p.POSITION_CONTROL, targetVelocity=10)
-# p.setJointMotorControl2(boxId, 23, p.POSITION_CONTROL, targetVelocity=5)
-# p.setJointMotorControl2(boxId, 24, p.POSITION_CONTROL, targetVelocity=5)
-# p.setJointMotorControl2(boxId, 20, p.VELOCITY_CONTROL, targetVelocity=5)
-# p.setJointMotorControl2(robot, 23, p.POSITION_CONTROL, targetVelocity=5)
-p.setJointMotorControl2(robot, 8, p.VELOCITY_CONTROL, targetVelocity=-15)
-p.setJointMotorControl2(robot, 7, p.VELOCITY_CONTROL, targetVelocity=-15)
-p.setRealTimeSimulation(1)
-
-while 1:
-    start()
-    time.sleep(1. / 240.)
+if __name__ == '__main__':
+    agv = mir_with_ur()
+    agv.run()
---
+++ Slam_fuction.py
@@ -0,0 +1,273 @@
+import numpy as np
+import math
+from copy import deepcopy
+
+Q = np.diag([3.0, np.deg2rad(10.0)]) ** 2
+R = np.diag([1.0, np.deg2rad(20.0)]) ** 2
+
+#  Simulation parameter
+Qsim = np.diag([0.3, np.deg2rad(2.0)]) ** 2
+Rsim = np.diag([0.5, np.deg2rad(10.0)]) ** 2
+OFFSET_YAWRATE_NOISE = 0.01
+
+DT = 0.1  # time tick [s]
+SIM_TIME = 50.0  # simulation time [s]
+MAX_RANGE = 20.0  # maximum observation range
+M_DIST_TH = 2.0  # Threshold of Mahalanobis distance for data association.
+STATE_SIZE = 3  # State size [x,y,yaw]
+LM_SIZE = 2  # LM srate size [x,y]
+N_PARTICLE = 100  # number of particle
+NTH = N_PARTICLE / 1.5  # Number of particle for re-sampling
+
+
+class Particle:
+
+    def __init__(self, N_LM):
+        self.w = 1.0 / N_PARTICLE
+        self.x = 0.0
+        self.y = 0.0
+        self.yaw = 0.0
+        # landmark x-y positions
+        self.lm = np.zeros((N_LM, LM_SIZE))
+        # landmark position covariance
+        self.lmP = np.zeros((N_LM * LM_SIZE, LM_SIZE))
+
+
+def motion_model(x, u):
+    F = np.array([[1.0, 0, 0],
+                  [0, 1.0, 0],
+                  [0, 0, 1.0]])
+
+    B = np.array([[DT * math.cos(x[2, 0]), 0],
+                  [DT * math.sin(x[2, 0]), 0],
+                  [0.0, DT]])
+    x = F @ x + B @ u
+
+    x[2, 0] = Particle.pi_2_pi(x[2, 0])
+    return x
+
+
+def predict_particles(particles, u):
+    for i in range(N_PARTICLE):
+        px = np.zeros((STATE_SIZE, 1))
+        px[0, 0] = particles[i].x
+        px[1, 0] = particles[i].y
+        px[2, 0] = particles[i].yaw
+        ud = u + (np.random.randn(1, 2) @ R).T  # add noise
+        px = Particle.motion_model(px, ud)
+        particles[i].x = px[0, 0]
+        particles[i].y = px[1, 0]
+        particles[i].yaw = px[2, 0]
+
+    return particles
+
+
+def pi_2_pi(angle):
+    return (angle + math.pi) % (2 * math.pi) - math.pi
+
+
+N_LM = 0
+particles = [Particle(N_LM) for i in range(N_PARTICLE)]
+time = 0.0
+v = 1.0  # [m/s]
+yawrate = 0.1  # [rad/s]
+u = np.array([v, yawrate]).reshape(2, 1)
+history = []
+while SIM_TIME >= time:
+    time += DT
+    particles = Particle.predict_particles(particles, u)
+    history.append(deepcopy(particles))
+
+
+def observation(xTrue, xd, u, RFID):
+    # calc true state
+    xTrue = motion_model(xTrue, u)
+
+    # add noise to range observation
+    z = np.zeros((3, 0))
+    for i in range(len(RFID[:, 0])):
+
+        dx = RFID[i, 0] - xTrue[0, 0]
+        dy = RFID[i, 1] - xTrue[1, 0]
+        d = math.sqrt(dx ** 2 + dy ** 2)
+        angle = pi_2_pi(math.atan2(dy, dx) - xTrue[2, 0])
+        if d <= MAX_RANGE:
+            dn = d + np.random.randn() * Qsim[0, 0]  # add noise
+            anglen = angle + np.random.randn() * Qsim[1, 1]  # add noise
+            zi = np.array([dn, pi_2_pi(anglen), i]).reshape(3, 1)
+            z = np.hstack((z, zi))
+
+    # add noise to input
+    ud1 = u[0, 0] + np.random.randn() * Rsim[0, 0]
+    ud2 = u[1, 0] + np.random.randn() * Rsim[1, 1] + OFFSET_YAWRATE_NOISE
+    ud = np.array([ud1, ud2]).reshape(2, 1)
+
+    xd = motion_model(xd, ud)
+
+    return xTrue, z, xd, ud
+
+
+def update_with_observation(particles, z):
+    for iz in range(len(z[0, :])):
+
+        lmid = int(z[2, iz])
+
+        for ip in range(N_PARTICLE):
+            # new landmark
+            if abs(particles[ip].lm[lmid, 0]) <= 0.01:
+                particles[ip] = add_new_lm(particles[ip], z[:, iz], Q)
+            # known landmark
+            else:
+                w = compute_weight(particles[ip], z[:, iz], Q)
+                particles[ip].w *= w
+                particles[ip] = update_landmark(particles[ip], z[:, iz], Q)
+
+    return particles
+
+
+def compute_weight(particle, z, Q):
+    lm_id = int(z[2])
+    xf = np.array(particle.lm[lm_id, :]).reshape(2, 1)
+    Pf = np.array(particle.lmP[2 * lm_id:2 * lm_id + 2])
+    zp, Hv, Hf, Sf = compute_jacobians(particle, xf, Pf, Q)
+    dx = z[0:2].reshape(2, 1) - zp
+    dx[1, 0] = pi_2_pi(dx[1, 0])
+
+    try:
+        invS = np.linalg.inv(Sf)
+    except np.linalg.linalg.LinAlgError:
+        print("singuler")
+        return 1.0
+
+    num = math.exp(-0.5 * dx.T @ invS @ dx)
+    den = 2.0 * math.pi * math.sqrt(np.linalg.det(Sf))
+    w = num / den
+
+    return w
+
+
+def compute_jacobians(particle, xf, Pf, Q):
+    dx = xf[0, 0] - particle.x
+    dy = xf[1, 0] - particle.y
+    d2 = dx ** 2 + dy ** 2
+    d = math.sqrt(d2)
+
+    zp = np.array(
+        [d, pi_2_pi(math.atan2(dy, dx) - particle.yaw)]).reshape(2, 1)
+
+    Hv = np.array([[-dx / d, -dy / d, 0.0],
+                   [dy / d2, -dx / d2, -1.0]])
+
+    Hf = np.array([[dx / d, dy / d],
+                   [-dy / d2, dx / d2]])
+
+    Sf = Hf @ Pf @ Hf.T + Q
+
+    return zp, Hv, Hf, Sf
+
+
+def add_new_lm(particle, z, Q):
+    r = z[0]
+    b = z[1]
+    lm_id = int(z[2])
+
+    s = math.sin(pi_2_pi(particle.yaw + b))
+    c = math.cos(pi_2_pi(particle.yaw + b))
+
+    particle.lm[lm_id, 0] = particle.x + r * c
+    particle.lm[lm_id, 1] = particle.y + r * s
+
+    # covariance
+    Gz = np.array([[c, -r * s],
+                   [s, r * c]])
+
+    particle.lmP[2 * lm_id:2 * lm_id + 2] = Gz @ Q @ Gz.T
+
+    return particle
+
+
+def update_KF_with_cholesky(xf, Pf, v, Q, Hf):
+    PHt = Pf @ Hf.T
+    S = Hf @ PHt + Q
+
+    S = (S + S.T) * 0.5
+    SChol = np.linalg.cholesky(S).T
+    SCholInv = np.linalg.inv(SChol)
+    W1 = PHt @ SCholInv
+    W = W1 @ SCholInv.T
+
+    x = xf + W @ v
+    P = Pf - W1 @ W1.T
+
+    return x, P
+
+
+def update_landmark(particle, z, Q):
+    lm_id = int(z[2])
+    xf = np.array(particle.lm[lm_id, :]).reshape(2, 1)
+    Pf = np.array(particle.lmP[2 * lm_id:2 * lm_id + 2, :])
+
+    zp, Hv, Hf, Sf = compute_jacobians(particle, xf, Pf, Q)
+
+    dz = z[0:2].reshape(2, 1) - zp
+    dz[1, 0] = pi_2_pi(dz[1, 0])
+
+    xf, Pf = update_KF_with_cholesky(xf, Pf, dz, Q, Hf)
+
+    particle.lm[lm_id, :] = xf.T
+    particle.lmP[2 * lm_id:2 * lm_id + 2, :] = Pf
+
+    return particle
+
+
+def normalize_weight(particles):
+    sumw = sum([p.w for p in particles])
+
+    try:
+        for i in range(N_PARTICLE):
+            particles[i].w /= sumw
+    except ZeroDivisionError:
+        for i in range(N_PARTICLE):
+            particles[i].w = 1.0 / N_PARTICLE
+
+        return particles
+
+    return particles
+
+
+def resampling(particles):
+    """
+    low variance re-sampling
+    """
+
+    particles = normalize_weight(particles)
+
+    pw = []
+    for i in range(N_PARTICLE):
+        pw.append(particles[i].w)
+
+    pw = np.array(pw)
+
+    Neff = 1.0 / (pw @ pw.T)  # Effective particle number
+
+    if Neff < NTH:  # resampling
+        wcum = np.cumsum(pw)
+        base = np.cumsum(pw * 0.0 + 1 / N_PARTICLE) - 1 / N_PARTICLE
+        resampleid = base + np.random.rand(base.shape[0]) / N_PARTICLE
+
+        inds = []
+        ind = 0
+        for ip in range(N_PARTICLE):
+            while (ind < wcum.shape[0] - 1) and (resampleid[ip] > wcum[ind]):
+                ind += 1
+            inds.append(ind)
+
+        tparticles = particles[:]
+        for i in range(len(inds)):
+            particles[i].x = tparticles[inds[i]].x
+            particles[i].y = tparticles[inds[i]].y
+            particles[i].yaw = tparticles[inds[i]].yaw
+            particles[i].w = 1.0 / N_PARTICLE
+
+    return particles, inds
+
